f731bb42c3ec7f5bdb3d604dffd3a3838a4cf960
Punit Lad <punitlad@gmail.com>
2026-04-04 16:29:37 -0400

    create extractor to pull updates into repository

---
+++ extract.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cp ~/.gitconfig_personal gitconfig_personal
+cp ~/.gitconfig gitconfig
+cp ~/.gitignore_global gitignore_global
+cp ~/.personalrc personalrc
+cp ~/Library/Application\ Support/VSCodium/User/settings.json vscodesettings.json
+cp ~/.ssh/config sshconfig
+cp ~/.config/1Password/ssh/agent.toml 1passwordagent.toml
